7d8660958d564ecaa59ddeb910784e8db7ebe434
Marina <77115406+maripaixao@users.noreply.github.com>
2021-08-19 18:00:44 -0300

    Add files via upload

---
+++ Projetos-b7web/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="../assets-bs/js-css/bootstrap.min.css" rel="stylesheet">
+        <script src="../assets-bs/js-css/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="assets/style.css">
+        <title>Projetos B7Web - Desafio 7 Projetos em 7 Dias</title>
+    </head>
+    <body style="background-color: #555">
+        <div class="container">
+            <div class="offset-md-5 margin-top-50">
+                <h1 style="margin-left:12px; color: white">Projetos</h1>
+            </div>
+            <div class="row margin-top-80">
+                 
+               <div class="offset-md-3 col-md-6">
+
+                            <div class="row margin-top-50">
+                            <div class="col-md-4">
+                                <button class="btn btn-dark col-md-10" onclick="location.href='pj-01/index.html'">Projeto 01 - Bateria</button>
+                                
+                            </div>
+
+                            <div class="col-md-4">
+                                <button class="btn btn-dark col-md-10" onclick="location.href='pj-02/index.html'">Projeto 02 - Relógio</button>
+                            </div>
+
+                            <div class="col-md-4">
+                                <button class="btn btn-dark col-md-10" onclick="location.href='pj-03/index.html'">Projeto 03 - Clima</button>
+                            </div>
+                            </div>
+                                
+                            <div class="row margin-top-50">
+                            <div class="col-md-4">
+                                <button class="btn btn-dark col-md-10" onclick="location.href='pj-04/index.html'">Projeto 04 - Jogo da Velha</button>
+                            </div>
+
+                            <div class="col-md-4">
+                                <button class="btn btn-dark col-md-10" onclick="location.href='pj-05/index.html'">Projeto 05 - Canvas</button>
+                            </div>
+                            
+                            <div class="col-md-4">
+                                <button class="btn btn-dark col-md-10" onclick="location.href='pj-06/index.html'">Projeto 06 - Quiz</button>
+                            </div>
+                            </div>
+                            
+                            <div class="col-md-4 margin-top-50 offset-md-4">
+                                <button class="btn btn-dark col-md-10" onclick="location.href='pj-07/index.html'">Projeto 07 - Arrastar e Soltar</button>
+                            </div>
+
+                </div>
+            </div>
+            
+        </div>
+    </body>
+    </body>
+</html>
